3bb3adf6d02d7a98b4fd0e73fb076339d5a36489
edyta2770 <edyta.elert27@gmail.com>
2022-07-23 15:58:52 +0200

    car

---
+++ src/Car.java
@@ -0,0 +1,20 @@
+public class Car {
+    private String colour;
+    private int doorsAmount;
+    private String brand;
+    private boolean automaticgearbox;
+
+    public Car (String colour, int doorsAmount, String brand, boolean automaticGearbox) {
+        this.colour = colour;
+        this.doorsAmount = doorsAmount;
+        this.brand = brand;
+        this.automaticgearbox = automaticGearbox;
+    }
+public void setAutomaticgearbox (boolean automaticgearbox){
+        this.automaticgearbox = automaticgearbox;
+}
+public boolean getAutomaticGearbox(){
+        return automaticgearbox;
+}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Car audi = new Car( "Red", 5, "A4",  false);
+        System.out.println(audi.getAutomaticGearbox());
+        audi.setAutomaticgearbox(true);
+        System.out.println(audi.getAutomaticGearbox());
+
+    }
+
+}
